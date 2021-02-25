16b25e7ec6e49405690378679f734f197a5fc886
Christian <927.christian@gmail.com>
2021-02-25 17:06:24 -0600

    added server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const mongoose = require ("mongoose");
+
+const PORT = process.env.PORT || 3030; 
+
+const app = expres(); 
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", {
+    useNewUrlParser: true, 
+    useFindAndModify: false
+});
+
+
+//routes 
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
