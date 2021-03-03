5c2a792176d992e5a0ec71c37e6bf427179d2408
Christian <927.christian@gmail.com>
2021-03-02 21:16:00 -0600

    modified database name

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", {
+mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
